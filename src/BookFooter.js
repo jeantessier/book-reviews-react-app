f95c44d9d5970e5c0ec675985a00482f612427a9
Jean Tessier <jean@jeantessier.com>
2019-11-29 21:34:42 -0800

    Change footer for backlog entries

--- src/BookFooter.js
+++ src/BookFooter.js
@@ -5,20 +5,20 @@ export default function BookFooter(props) {
 
     return(
         <footer>
-            <table className="reading-metadata">
-                <tbody>
-                <tr>
-                    <td className="time">Started reading:</td>
-                    {start ? <td className="time"><code>{start}</code></td> : <td className="no_time">not started</td>}
-                </tr>
-                {props.start &&
+            {start &&
+                <table className="reading-metadata">
+                    <tbody>
+                    <tr>
+                        <td className="time">Started reading:</td>
+                        {start ? <td className="time"><code>{start}</code></td> : <td className="no_time">not started</td>}
+                    </tr>
                     <tr>
                         <td className="time">Finished reading:</td>
                         {stop ? <td className="time"><code>{stop}</code></td> : <td className="no_time">in progress</td>}
                     </tr>
-                }
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            }
         </footer>
     )
 }
--- src/Contents.js
+++ src/Contents.js
@@ -8,7 +8,7 @@ export default function Contents(props) {
             <p>{isLoading ? "Loading..." : "Contents"}</p>
             <div className="table-of-contents">
                 {books.map(book => (
-                    <div key={book.name}><span className="marker" dangerouslySetInnerHTML={{__html: book.start ? book.start.substring(0, 4) : "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" }}/> <a href={'#' + book.name} dangerouslySetInnerHTML={{__html: book.titles[0].title.split(':', 1)[0] }} /></div>
+                    <div key={book.name}><span className="marker" dangerouslySetInnerHTML={{__html: (book.start ? book.start : book.acquired).substring(0, 4)}}/> <a href={'#' + book.name} dangerouslySetInnerHTML={{__html: book.titles[0].title.split(':', 1)[0] }} /></div>
                 ))}
             </div>
         </div>
