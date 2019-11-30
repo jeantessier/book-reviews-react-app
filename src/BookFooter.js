75436797c515da3c217af2db785b076931787f96
Jean Tessier <jean@jeantessier.com>
2019-11-29 18:32:03 -0800

    Better way to do conditional in React

--- src/BookFooter.js
+++ src/BookFooter.js
@@ -11,12 +11,11 @@ export default function BookFooter(props) {
                     <td className="time">Started reading:</td>
                     {start ? <td className="time"><code>{start}</code></td> : <td className="no_time">not started</td>}
                 </tr>
-                {props.start ?
+                {props.start &&
                     <tr>
                         <td className="time">Finished reading:</td>
                         {stop ? <td className="time"><code>{stop}</code></td> : <td className="no_time">in progress</td>}
                     </tr>
-                    : null
                 }
                 </tbody>
             </table>
