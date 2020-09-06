f57c51b821c2d3b6974594efaea8dc351b01dcfa
Jean Tessier <jean@jeantessier.com>
2020-09-05 21:16:37 -0700

    Fix tag composition

--- src/BookHeader.js
+++ src/BookHeader.js
@@ -7,13 +7,15 @@ export default function BookHeader(props) {
         <header>
             <table className="book-metadata">
                 <tbody>
-                <td className="author">
-                    {authors.map((author, index) => <div key={index} dangerouslySetInnerHTML={{__html: author }}/>)}
-                </td>
-                <td className="publisher" dangerouslySetInnerHTML={{__html: publisher }}/>
-                <td className="published-year">
-                    {years.map((year, index) => <div key={index} dangerouslySetInnerHTML={{__html: year }}/>)}
-                </td>
+                    <tr>
+                        <td className="author">
+                            {authors.map((author, index) => <div key={index} dangerouslySetInnerHTML={{__html: author }}/>)}
+                        </td>
+                        <td className="publisher" dangerouslySetInnerHTML={{__html: publisher }}/>
+                        <td className="published-year">
+                            {years.map((year, index) => <div key={index} dangerouslySetInnerHTML={{__html: year }}/>)}
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </header>
