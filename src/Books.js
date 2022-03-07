eb26de47cfe35b0dab7a6da3eff2562fc37cc98f
Jean Tessier <jean@jeantessier.com>
2022-03-06 16:49:05 -0800

    Use object destructuring for long list of props

--- src/Books.js
+++ src/Books.js
@@ -4,17 +4,7 @@ import Book from './Book';
 export default function Books({ books }) {
     return (
         books.map(book => (
-            <Book
-                key={book.name}
-                name={book.name}
-                titles={book.titles}
-                authors={book.authors}
-                publisher={book.publisher}
-                years={book.years}
-                body={book.body}
-                start={book.start}
-                stop={book.stop}
-            />
+            <Book key={book.name} {...book} />
         ))
     )
 }
--- src/Contents.js
+++ src/Contents.js
@@ -6,7 +6,17 @@ export default function Contents({ books, isLoading }) {
             <p>{isLoading ? "Loading..." : "Contents"}</p>
             <div className="table-of-contents">
                 {books.map(book => (
-                    <div key={book.name}><span className="marker" dangerouslySetInnerHTML={{__html: (book.start ? book.start : book.acquired).substring(0, 4)}}/> <a href={'#' + book.name} dangerouslySetInnerHTML={{__html: book.titles[0].title.split(':', 1)[0] }} /></div>
+                    <div key={book.name}>
+                        <span
+                            className="marker"
+                            dangerouslySetInnerHTML={{__html: (book.start ? book.start : book.acquired).substring(0, 4)}}
+                        />
+                        &nbsp;
+                        <a
+                            href={'#' + book.name}
+                            dangerouslySetInnerHTML={{__html: book.titles[0].title.split(':', 1)[0] }}
+                        />
+                    </div>
                 ))}
             </div>
         </div>
