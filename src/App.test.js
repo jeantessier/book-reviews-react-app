a5a1b501a766cf025f52dd5cdd121e58e228429d
Jean Tessier <jean@jeantessier.com>
2023-04-21 11:05:35 -0700

    Upgrade dependencies
        $ yarn upgrade
    
    I had to `rm -fR node_modules` and run `yarn` again to get past some
    Jest parsing problem (and add to `transformIgnorePatterns`).
    
    I rewrote the one test based on the latest default test generated by
    `create-react-app`.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders the default title', () => {
+  render(<App />);
+  const titleElement = screen.getByText(/Readings/);
+  expect(titleElement).toBeInTheDocument();
 });
