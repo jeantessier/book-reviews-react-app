587c6e487f41f78fa2ce3ed4214e39f74023dbff
Jean Tessier <jean@jeantessier.com>
2023-09-13 21:08:27 -0700

    Show and hide edit history
    The implementation is taken from section 6.2.3 of _React Hooks in
    Action_ pn page 145.

--- src/BookHistory.js
+++ src/BookHistory.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './BookHistory.css';
 
 export default function BookHistory({ history }) {
+    const [showHistory, setShowHistory] = useState(false)
+
+    function toggleShowHistory() {
+        setShowHistory(prev => !prev)
+    }
+
     return (
-        <table className="history">
-            <caption>History</caption>
-            <tbody>
-                {history.map(entry => (
-                    <tr>
-                        <td className="time" dangerouslySetInnerHTML={{__html: entry.date }}/>
-                        <td>{entry.message}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <div className="history">
+            <span onClick={toggleShowHistory}>{showHistory ? '-' : '+'}</span>
+            {showHistory &&
+                <table>
+                    <caption>History</caption>
+                    {showHistory &&
+                        <tbody>
+                        {history.map(entry => (
+                            <tr>
+                                <td className="time" dangerouslySetInnerHTML={{__html: entry.date }}/>
+                                <td>{entry.message}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    }
+                </table>
+            }
+        </div>
     )
 }
