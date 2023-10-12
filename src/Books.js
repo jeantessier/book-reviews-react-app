7f7ad01fa86064999a2ef6e4a1d0d1fa81c34292
Jean Tessier <jean@jeantessier.com>
2023-10-11 20:40:01 -0700

    Add separator between entries
    The separator is a faint horizontal line.

--- src/Books.js
+++ src/Books.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Books.css';
 import Book from './Book';
 
 export default function Books({ books }) {
     return (
-        books.map(book => (
-            <Book key={book.name} {...book} />
+        books.map((book, index) => (
+            <div>
+                {index !== 0 && <div className="separator"></div>}
+                <Book key={book.name} {...book} />
+            </div>
         ))
     )
 }
