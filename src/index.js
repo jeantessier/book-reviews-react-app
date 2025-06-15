739dd8ffbf852f02bacbb3df475fb7cbca63b9fd
Jean Tessier <jean@jeantessier.com>
2025-06-15 16:22:33 -0700

    Update dependencies
        yarn up -R "*"
    
    Replace `render()` with `createRoot()`.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import * as ReactDOM from 'react-dom/client'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import './index.css'
 
 const dataUrlFromPathname = pathname => pathname.includes('.html') ? dataUrlFromFilename(pathname) : dataUrlFromDirname(pathname)
 const dataUrlFromFilename = filename => filename.replace(/\.html.*/, '.json')
@@ -10,10 +9,9 @@ const dataUrlFromDirname = dirname => (dirname.endsWith('/') ? dirname : dirname
 
 const dataUrl = dataUrlFromPathname(document.location.pathname)
 
-ReactDOM.render(
-    <App url={dataUrl}/>,
+ReactDOM.createRoot(
     document.getElementById('root')
-)
+).render(<App url={dataUrl}/>)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
